b8daa2858f45af5276b52b1ae2446aaf42036c4d
JJJHolscher <jochem.holscher@outlook.com>
2019-03-27 17:37:24 +0100

    The reddit search can now construct a vector array

--- reddit_search.py
+++ reddit_search.py
@@ -1,18 +1,65 @@
+""" This program scrapes the top posts of reddit and puts them in a json file.
+
+the database (named 'DB' throughout this file) is a dictionary of postID keys
+mapped with posts:
+DB = {
+    postID_0 : post_0,
+    postID_1 : post_1,
+    ...
+    postID_last : post_last
+}
+
+A post is stored like this:
+[
+    title,                 # string
+    name_of_subreddit,     # string
+    upvote_ratio,          # float
+    score,                 # int
+    selftext,              # string
+    
+    [
+        comment_0,
+        comment_1,
+        ...
+        comment_last
+    ]
+]
+
+Each comment looks like this:
+[
+    score,                 # int
+    body,                  # string
+
+    [
+        subcomment_0,
+        subcomment_1,
+        ...
+        subcomment_last
+    ]
+]    
+"""
+
 import praw
 import json
 from praw.models import MoreComments
+from numpy import array
 
 # config file contains personal information and should be handled with care
 import config
 from general_easyness import lprint, minput
+from prepare_string import prep, additive, multiplicative
 
 
 DB_FILE = "reddit_db.json"
+VA_FILE = "reddit_va.csv"
 
 SUBREDDIT = ""
 LIMIT = 100
 T = "all"
 
+DB = dict()
+INDEX = None
+VECTORS = []
 
 reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret,
                      password=config.password, user_agent=config.user_agent,
@@ -52,75 +99,138 @@ def format_post(post):
     return post.id, content
 
 
-def db_add(db, post):
+def db_add(post):
     key, content = format_post(post)
-    db[key] = content
+    DB[key] = content
     return key
 
 
-def db_load(filename):
-    db = {}
+def db_load(filename=DB_FILE):
+    DB = {}
+    INDEX = None
 
+    print("reddit_search.py:\n\tloading database...", end="", flush=True)
     try:
         with open(filename) as f:
-            index = f.readline()[:-1]
-            if not index:
-                index = None
-            db = json.loads(f.readline())
+            INDEX = f.readline()[:-1]
+            if not INDEX:
+                INDEX = None
+            DB = json.loads(f.readline())
+        print("done.")
 
     except IOError:
-        index = None
+        print("\n\tno database found at", DB_FILE)
+        INDEX = None
 
-    return db, index
+    return DB, INDEX
 
 
-def db_store(db, filename, index=None):
+def db_store(filename=DB_FILE):
     with open(filename, "w") as f:
-        if index:
-            f.write(index)
-        f.write('\n' + json.dumps(db))
+        if INDEX:
+            f.write(INDEX)
+        f.write('\n' + json.dumps(DB))
+    
+    va_store()
 
 
-def scrape(db, after=None):
+def va_load(filename=VA_FILE):
+    global VECTORS
+    VECTORS = []
+
+    print("\tloading the vector array...", end="", flush=True)
+    try:
+        with open(filename) as f:
+            for line in f:
+                add_vec, mul_vec, postID = line.split(",")
+
+                add_vec = array([float(n) for n in add_vec.split(' ')])
+                mul_vec = array([float(n) for n in mul_vec.split(' ')])
+                VECTORS.append((add_vec, mul_vec, postID[:-1]))
+
+    except IOError:
+        print("\n\tno vector array found at", VA_FILE,
+              "constructing a new one...", end="", flush=True)
+        
+        for postID, post in DB.items():
+            arr = prep(post[0] + post[4])
+            add_vec, mul_vec = additive(arr), multiplicative(arr)
+            VECTORS.append((add_vec, mul_vec, postID))
+
+        print("storing...", end="", flush=True)
+        va_store(filename)
+
+    print("done.")
+    return VECTORS
+
+
+def va_store(filename=VA_FILE):
+    with open(filename, 'w') as f:
+        for add_vec, mul_vec, postID in VECTORS:
+            add_str, mul_str = "", ""
+
+            for i, n in enumerate(add_vec):
+                add_str += str(n) + " "
+                mul_str += str(mul_vec[i]) + " "
+
+            f.write(add_str[:-1] + ',' + mul_str[:-1] + ',' + postID + '\n')
+
+
+def scrape():
+    global INDEX
+
     p = {
         "limit" : LIMIT,
         "t" : T
     }
-    if after:
-        p["after"] = "t3_" + after
+    if INDEX:
+        p["after"] = "t3_" + INDEX
 
-    index = None
     for i, post in enumerate(reddit.get("/top", params=p)):
         if i % 10 == 0:
             print(".", end="", flush=True)
-        index = db_add(db, post)
+        INDEX = db_add(post)
     
-    return index
+    return INDEX
 
 
-def example():
-    """ Example code on how to use the functions in this program.
+def get(postID):
+    """ return [
+            post.text, [comment0.body, ..., comment_last.body], 
+            post.score, [comment0.score, ..., comment_last.score]
+        ]
     """
-    db = db_load(DB_FILE)
 
-    SUBREDDIT = input("hi there, whose opinion do you want to know: ")
-    query = input("what do you want to ask them: ")
-    index = None
-    for post in search(query):
-        index = db_add(db, post)
+    post = DB[postID]
+    out = [post[0] + post[-2], [], post[-3], []]
+    
+    for comment in DB[postID][-1]:
+        body, score = comment[1], comment[0]
+        out[1].append(body)
+        out[3].append(score)
 
-    print("The database now consists of", len(db), "posts.")
-    db_store(db, DB_FILE, index)
+    return out
 
+
+""" ToDo:
+Database of [[add_vec, mult_vec, postID],
+             [  ''   ,    ''   ,   ''  ],
+             [  ''   ,    ''   ,   ''  ]]   
+"""
+
+
+DB, INDEX = db_load()
+if DB:
+    VECTORS = va_load()
+print()
 
 if __name__ == '__main__':
-    print("Load database...", end="", flush=True)
-    db, index = db_load(DB_FILE)
-    print(" Done\t\t\tlatest post =", index)
+
+    print("Latest post =", INDEX)
 
     while True:
         print("Scraping", end="")
-        index = scrape(db, index)
+        scrape()
         print(" Saving...", end="", flush=True)
-        db_store(db, DB_FILE, index)
-        print(" Done\tLatest post =", index)
+        db_store()
+        print(" Done\tLatest post =", INDEX)
